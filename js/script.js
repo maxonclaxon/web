215a47f590022de2d1ef9a5c256ea60ba5dbd944
maxim <satamaks@gmail.com>
2020-02-27 18:35:11 +0300

    2

--- js/script.js
+++ js/script.js
@@ -6,9 +6,25 @@ $(document).ready(function () {
 $(window).on('scroll',function(){
     var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     if(posTop>100){
-        $('#navmenu').css({'background-color':'rgba(0,0,0,0.8)','transition-duration':'1.0s','border-bottom':'1px solid white'});
+        $('#navmenu').css({
+            'height':'60px',
+            'background-color':'rgba(0,0,0,0.8)','border-bottom':'1px solid white','transition-duration':'1.0s'
+        });
+        $('#navmenu>.container').css({
+            'margin-top':'0px',
+            'margin-bottom':'0px',
+            'transition-duration':'1.0s'
+        })
     }
     else{
-        $('#navmenu').css({'background-color':'inherit','transition-duration':'1.0s'});
+        $('#navmenu').css({
+            'height':'100px',
+            'background-color':'rgba(0,0,0,0.0)','border-bottom':'1px solid rgba(0,0,0,0)','transition-duration':'1.0s'
+        });
+        $('#navmenu>.container').css({
+            'margin-top':'20px',
+            'margin-bottom':'20px',
+            'transition-duration':'1.0s'
+        })
     }
 });
