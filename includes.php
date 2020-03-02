73e7b65d73b12531804539ec760bc474e9af082f
maxshmax <satamaks@gmail.com>
2020-03-02 14:57:12 +0300

    4th

--- includes.php
+++ includes.php
@@ -1,3 +1,4 @@
 <script src="./js/jquery-3.4.1.min.js"></script>
 <script src="./js/script.js"></script>
 <link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
--- includes/contacts.php
+++ includes/contacts.php
@@ -1,5 +1,31 @@
 <div id="contacts">
     <div class="container">
-        <div></div>
+        <div id="left">
+         <div class="t1">Контакты</div>
+         <div id="social">
+            <ul>
+                <li>
+                    <div class="social github">
+                        <a href="https://www.github.com/maxonclaxon" target="_blank"><i class="fa fa-github fa-2x"></i></a>
+                        <span class="tsoc t1">github</span>
+                    </div>
+                    <div class="social vk">
+                        <a href="https://www.vk.com/hasax" target="_blank"><i class="fa fa-vk fa-2x"></i></a>
+                        <span class="tsoc t1">vkontakte</span>
+                    </div>
+                    <div class="social telegram">
+                        <a href="#" target="_blank"><i class="fa fa-telegram fa-2x"></i></a>
+                        <span class="tsoc t1">telegram</span>
+                    </div>
+                    <div class="social instagram">
+                        <a href="https://www.instagram.com/syryx/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+                        <span class="tsoc t1">instagram</span>
+                    </div>
+                </li>
+            </ul>
+         </div>
+        </div>
+        <div id="right">
+        </div>
     </div>
 </div>
